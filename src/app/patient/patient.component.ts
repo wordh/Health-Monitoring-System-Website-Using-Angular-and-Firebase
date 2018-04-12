7d914724588eb8acc8102d0f07ab5dca2b8fbe8e
Wordh Ul Hasan <wordhulhasan@yahoo.com>
2018-04-13 00:29:15 +0600

    Add files via upload
    Patient Profile Updated with Graph. ECG and SPO2 Graph Working.

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -5,6 +5,8 @@ import { AuthService } from '../auth-service';
 import * as firebase from 'firebase';
 import {Router} from '@angular/router';
 import {NgForm} from '@angular/forms';
+import { Chart } from 'angular-highcharts';
+import {HIGHCHARTS_MODULES} from 'angular-highcharts';
 
 
 @Component({
@@ -28,10 +30,61 @@ export class PatientComponent implements OnInit {
       this.items.forEach(element => {
         let value= Object.keys(element).map(key=>element[key]);
          this.boltesi=value;
+         this.chart.addPoint(value[8]);
+         this.chart.removePoint(0);
+         this.chart2.addPoint(value[1]);
+         this.chart2.removePoint(0);
       });
       
     
   }
+
+
+  chart = new Chart({
+    
+    chart:{
+      animation: false,
+      type:'line'
+      
+    },
+
+    
+    
+    title:{
+      text:'SPO2'
+    },
+    credits:{
+      enabled:false
+    },
+    series:[{
+      name:'Pulse Rate',
+      
+      data:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    }]
+  });
+
+  chart2 = new Chart({
+    
+    chart:{
+      animation: false,
+      type:'line'
+    },
+    
+    title:{
+      text:'ECG'
+    },
+    credits:{
+      enabled:false
+    },
+    series:[{
+      name:'ECG',
+      
+      data:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    }]
+  });
+
+
+
   onSubmit(form: NgForm) {
     
     firebase.auth().signOut;
